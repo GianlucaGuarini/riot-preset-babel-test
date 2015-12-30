f69f7e6648ee83bb070eb73979657c602a951f92
Gianluca Guarini <gianluca.guarini@gmail.com>
2015-12-30 19:26:21 +0100

    using babel helpers

--- component.js
+++ component.js
@@ -3,13 +3,9 @@ riot.tag2('component', '<div> <p>{message}</p> </div>', '', '', function(opts) {
 
 var _moment = require('moment');
 
-var _moment2 = _interopRequireDefault(_moment);
+var _moment2 = babelHelpers.interopRequireDefault(_moment);
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } step("next"); }); }; }
-
-this.message = _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
+this.message = babelHelpers.asyncToGenerator(regeneratorRuntime.mark(function _callee() {
     return regeneratorRuntime.wrap(function _callee$(_context) {
         while (1) {
             switch (_context.prev = _context.next) {
